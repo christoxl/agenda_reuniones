23402fd3a9fc4c51337da11046ac88a8ddabf85b
Chris Perez <christoxl@gmail.com>
2017-09-29 11:34:29 -0500

    Added Slugify

--- web/models.py
+++ web/models.py
@@ -2,10 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Entidad(models.Model):
     nombre = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nombre)
+        super(Entidad, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.nombre
@@ -19,6 +25,11 @@ class Enlace(models.Model):
     apellido_pat = models.CharField(max_length=200)
     apellido_mat = models.CharField(max_length=200)
     cargo = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nombre)
+        super(Enlace, self).save(*args, **kwargs)
 
     def __str__(self):
         return (self.nombre + self.apellido_pat)
@@ -34,6 +45,11 @@ class Sesion(models.Model):
     fecha_prox = models.DateTimeField()
     entidad = models.ForeignKey(Entidad)
     enlaces = models.ManyToManyField(Enlace)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nombre)
+        super(Sesion, self).save(*args, **kwargs)
 
     def __str__(self):
         return (str(self.entidad) + ' - ' + self.nombre)
