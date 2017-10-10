e8fff0d2eef4ab295733aaed05fe5b1d4395c7c0
christoxl <christoxl@gmail.com>
2017-10-10 18:56:21 -0500

    Added Calendar

--- web/static/web/js/custom.js
+++ web/static/web/js/custom.js
@@ -3,3 +3,10 @@ $(function () {
         format:'DD-MM-YYYY HH:mm:ss'
     });
 });
+
+$(document).ready(function() {
+    // page is now ready, initialize the calendar...
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+});
