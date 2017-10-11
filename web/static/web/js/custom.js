9f5b69cd70f7ec8ff1832167ab62ad30aa2f5375
christoxl <christoxl@gmail.com>
2017-10-11 00:03:41 -0500

    Changes in custom.js

--- web/static/web/js/custom.js
+++ web/static/web/js/custom.js
@@ -6,6 +6,15 @@ $(function () {
 
 $(document).ready(function () {
     $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay,listWeek'
+        },
+        defaultDate: '2017-10-12',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true,
         events: '/api/'
     });
 });
